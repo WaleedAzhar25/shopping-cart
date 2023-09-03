e6ca02d2196daa640bb790a14e9f7a7bef2536bb
Waleed Azhar <waleedazhar903@gmail.com>
2023-09-03 19:49:55 +0500

    Initial

---
+++ lib/controllers/shopping_controller.dart
@@ -0,0 +1,28 @@
+import 'package:get/get.dart';
+
+import '../models/product.dart';
+
+class ShoppingController extends GetxController {
+  var products = <Product>[].obs;
+  //updating  process is here every information every event be getting inside!!!!!!!!!!!!!!!!
+
+  @override
+  void onInit() {
+    super.onInit();
+    fetchProducts();
+  }
+
+  void fetchProducts() async {
+    await Future.delayed(
+      const Duration(seconds: 1),
+    );
+    var productResult = [
+      Product(id: 1, price: 280),
+      Product(id: 2, price: 80),
+      Product(id: 3, price: 20),
+      Product(id: 4, price: 28),
+    ];
+
+    products.value = productResult;
+  }
+}
---
+++ lib/views/shopping_page.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../controllers/cart_controller.dart';
+import '../controllers/shopping_controller.dart';
+
+class ShoppingPage extends StatelessWidget {
+  ShoppingPage({Key? key}) : super(key: key);
+
+  final shoppingController = Get.put(ShoppingController());
+  final cartController = Get.put(CartController());
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton.extended(
+        backgroundColor: Colors.amber,
+        onPressed: () {},
+        icon: const Icon(Icons.shopping_cart, color: Colors.black),
+        label: GetX<CartController>(builder: (controller) {
+          return Text(controller.count.toString(),
+              style: const TextStyle(color: Colors.black));
+        }),
+      ),
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: GetX<ShoppingController>(builder: (controller) {
+                return ListView.builder(
+                  itemCount: controller.products.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Card(
+                      margin: const EdgeInsets.all(12),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      controller.products[index].productName,
+                                      style: const TextStyle(fontSize: 18),
+                                    ),
+                                    Text(
+                                      controller
+                                          .products[index].productDescription,
+                                      style: const TextStyle(fontSize: 16),
+                                    ),
+                                  ],
+                                ),
+                                Text(
+                                  "\$${controller.products[index].price.toString()}",
+                                  style: const TextStyle(fontSize: 14),
+                                ),
+                              ],
+                            ),
+                            ElevatedButton(
+                              onPressed: () {
+                                cartController
+                                    .addToCart(controller.products[index]);
+                              },
+                              child: const Text('Add to cart'),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                );
+              }),
+            ),
+            GetX<CartController>(builder: (controller) {
+              return Text(
+                'Total Amount: \$ ${controller.totalPrice}',
+                style: const TextStyle(color: Colors.white, fontSize: 18),
+              );
+            }),
+            const SizedBox(height: 100),
+          ],
+        ),
+      ),
+    );
+  }
+}
